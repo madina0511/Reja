644e92d1d5f6700eee4107d7b01c0aba9d637a45
madina0511 <madinkashakirovna@gmail.com>
2025-09-25 18:37:58 +0900

    fix:fixing  PORT

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ mongodb.connect(
       module.exports = client;
       const app = require("./app");
       const server = http.createServer(app);
-      let PORT = 3004;
+      let PORT = 3000;
       server.listen(PORT, function () {
         console.log(
           `the server is running successfully on port: ${PORT}, http://localhost:${PORT}`
